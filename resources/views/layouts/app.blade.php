f89333dedb17fe9ae702fb4c04c09d72c4e15d1a
RealZark <35647209@qq.com>
2020-03-16 14:15:25 +0800

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
+  @stack('styles')
+
 </head>
 
 <body>
@@ -34,6 +36,9 @@
 
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
+
+  @stack('scripts')
+
 </body>
 
 </html>
