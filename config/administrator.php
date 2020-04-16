6c3cfbd8dca122a509d1eca9f38a435a90ca8212
RealZark <35647209@qq.com>
2020-04-16 14:50:10 +0800

    内容管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -42,6 +42,12 @@ return [
             'roles',
             'permissions',
         ],
+
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
     ],
 
     /*
